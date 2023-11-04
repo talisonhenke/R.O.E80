5dd74b9f3d6cc449ad9c9e6b0e42366a622d9f08
Talison Henke <talison.henke@gmail.com>
2023-11-04 15:32:09 -0300

    botão de baixo

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -41,10 +41,10 @@
                 </ul>
             </div>
             <div class="contact text-center col-sm-12 col-md-8 col-lg-6 col-xl-6 mx-auto">
-                <a href="hhttps://wa.me/5553984734353?text=Ol%C3%A1,%20Rafael!%20Tudo%20bem?%20Gostaria%20de%20mais%20informa%C3%A7%C3%B5es%20sobre%20os%20apartamentos%20e%20casas%20do%20programa%20Minha%20casa,%20Miha%20vida." target="_blank" class="text-decoration-none text-black"><button type="button" class="btn btn-light contact-button gradient-button">Fale comigo no whatsapp!</button></a>
+                <a href="https://wa.me/5553984734353?text=Ol%C3%A1,%20Rafael!%20Tudo%20bem?%20Gostaria%20de%20mais%20informa%C3%A7%C3%B5es%20sobre%20os%20apartamentos%20e%20casas%20do%20programa%20Minha%20casa,%20Miha%20vida." target="_blank" class="text-decoration-none text-black"><button type="button" class="btn btn-light contact-button gradient-button">Fale comigo no whatsapp!</button></a>
                 <h2 class="text-white ouclass">OU</h2>
                 <p class="text-white text-page">Cadastre-se que eu mesmo entrarei em contato com você o quanto antes.</p>
-                <button type="button" data-bs-toggle="modal" data-bs-target="#exampleModal" class="btn btn-light contact-button gradient-button mb-4 mt-2">Envie-me um e-mail!</button>
+                <button type="button" data-bs-toggle="modal" data-bs-target="#exampleModal" class="btn btn-light contact-button gradient-button mb-4 mt-2">Envie-me sua dúvida que entrarei em contato!</button>
             </div>
 
             <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
